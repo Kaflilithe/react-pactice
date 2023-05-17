f3a69263654a37d770dcd8617790cd5507c0b6b6
daraivanova <kudiyarova.1975@gmail.com>
2023-05-18 01:05:04 +0300

    [Flex]: added base props

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import "./index.css";
 function App() {
   return (
     <div className="p-1">
-      <Flex orientation="column" justify="end" align="center">
+      <Flex
+        orientation="row"
+        justify="center"
+        align="center"
+        className="gap-4"
+        title="Fruits"
+        onClick={() => alert("Пошел нахуй")}
+      >
         <div>Ananas</div>
+        <div>Apple</div>
+        <div>Apple</div>
       </Flex>
     </div>
   );
--- src/components/template/Flex.tsx
+++ src/components/template/Flex.tsx
@@ -2,34 +2,33 @@ import classNames from "classnames";
 import { AlignItems } from "../../types/core/align.type";
 import { ParentComponent } from "../../types/core/base.type";
 import { JustifyItems } from "../../types/core/justify.type";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 type Props = {
   orientation?: "column" | "row" | "column-revers" | "row-reverse";
   justify?: JustifyItems;
   align?: AlignItems;
-};
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 export const Flex = (props: ParentComponent<Props>) => {
-  const classes = classNames("flex", {
-    "flex-col": props.orientation === "column",
-    "flex-row": props.orientation === "row",
-    "flex-col-reverse": props.orientation === "column-revers",
-    "flex-row-reverse": props.orientation === "row-reverse",
-    "justify-start": props.justify === "start",
-    "justify-end": props.justify === "end",
-    "justify-center": props.justify === "center",
-    "justify-stretch": props.justify === "stretch",
-    "items-start": props.align === "start",
-    "items-end": props.align === "end",
-    "items-center": props.align === "center",
-    "items-baseline": props.align === "baseline",
-    "items-stretch": props.align === "stretch"
+  const { orientation, justify, align, className, ...others } = props;
 
+  const classes = classNames("flex", {
+    [className || ""]: !!className,
+    "flex-col": orientation === "column",
+    "flex-row": orientation === "row",
+    "flex-col-reverse": orientation === "column-revers",
+    "flex-row-reverse": orientation === "row-reverse",
+    "justify-start": justify === "start",
+    "justify-end": justify === "end",
+    "justify-center": justify === "center",
+    "justify-stretch": justify === "stretch",
+    "items-start": align === "start",
+    "items-end": align === "end",
+    "items-center": align === "center",
+    "items-baseline": align === "baseline",
+    "items-stretch": align === "stretch",
   });
 
-  return (
-    <div className={classes}>
-      {props.children}
-    </div>
-  );
+  return <div className={classes} {...others} />;
 };
